ed59f148220a55411fbb76b61ad3ac172b65dfcb
lyaysan4 <lyaisan4@mail.ru>
2017-10-17 18:18:21 +0300

    Добавлена возможность добавления книг

--- src/test/java/spring/prog/springprog/SpringProgApplicationTests.java
+++ src/test/java/spring/prog/springprog/SpringProgApplicationTests.java
@@ -1,16 +1,44 @@
 package spring.prog.springprog;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import spring.prog.springprog.model.Author;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class SpringProgApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
+    @Test
+    public void name() throws Exception {
+        List<String> authorNames = new ArrayList<>();
+
+        authorNames.add("");
+        authorNames.add("");
+        authorNames.add("e");
+        authorNames.add("d");
+        authorNames.add("a");
+
+        List<String> list = authorNames.stream()
+                .filter(name -> {
+                    boolean empty = !name.isEmpty();
+                    System.out.println(empty);
+                    return empty;
+                })
+                .distinct()
+                .map(Author::new)
+                .sorted(Comparator.comparing(Author::getName))
+                .map(Author::getName)
+                .collect(toList());
+        System.out.println(list);
+    }
 }
